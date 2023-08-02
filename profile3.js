9a59713da754f1db946b2371f7d2eba546ba8afd
{shingo_uemoto} <{fumfum00z2@gmail.com}>
2023-08-03 02:41:51 +0900

    0.5

--- profile3.js
+++ profile3.js
@@ -1,31 +1,21 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-
-function fadeInOnScroll() {
-    const containers = document.querySelectorAll('.image-container');
-
-    containers.forEach(container => {
-        const containerTop = container.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-
-        if (containerTop < windowHeight - 100) {
-            container.classList.add('fade-in');
-        } else {
-            container.classList.remove('fade-in');
-        }
+function checkVisibility() {
+    var images = document.querySelectorAll(".img");
+    images.forEach(function (image) {
+      var imageTop = image.getBoundingClientRect().top;
+      var imageBottom = image.getBoundingClientRect().bottom;
+      var windowHeight = window.innerHeight;
+  
+      if (imageTop < windowHeight && imageBottom > 0) {
+        image.classList.add("visible");
+      } else {
+        image.classList.remove("visible");
+      }
     });
-}
-
-// ウィンドウのスクロールイベントに関数をバインド
-window.addEventListener('scroll', fadeInOnScroll);
-
-// ページ読み込み時にもフェードインを実行
-window.addEventListener('load', fadeInOnScroll);
-
-// 初期表示時にすぐにフェードインを適用
-fadeInOnScroll();
-document.getElementById('profile-image').addEventListener('click', function() {
-    window.location.href = "minae.html";
-});
-
-});
+  }
+  
+  // スクロールイベントのリスナー
+  window.addEventListener("scroll", checkVisibility);
+  
+  // 初回ロード時にもチェック
+  checkVisibility();
+  
